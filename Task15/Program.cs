d4ed81be2966e25121e2f186dfceb20eb53be7d0
Toivo <xan.asur@mail.ru>
2023-07-08 07:00:44 +0300

    Added Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -2,3 +2,18 @@
 обозначающую день недели, и проверяет
 является ли этот день выходным.*/
 
+Console.Write("Введите цифру обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 0 || number > 7)
+{
+    Console.WriteLine("Это не день недели.");
+}
+else if (number >= 6 && number <= 7)
+{
+    Console.WriteLine("Это выходной.");
+}
+else 
+{
+    Console.WriteLine("Это рабочий день.");
+}

85ca28f961d4dc83e6ee58dc62273840cefeca6f
Toivo <xan.asur@mail.ru>
2023-07-08 06:48:28 +0300

    Added Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -10,11 +10,16 @@ if (number < 0)
 {
     estNumber *= -1;
 }
+
+while (estNumber > 999)
+{
+    estNumber = estNumber / 10;
+}
 if (number < 100 && number > -100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else 
 {
-    Console.WriteLine($"Третья цифра числа {number} -> {(estNumber / 100) % 10}");
+    Console.WriteLine($"Третья цифра числа {number} -> {estNumber % 10}");
 }

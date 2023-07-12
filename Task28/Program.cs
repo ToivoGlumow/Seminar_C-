cfff1a6a965a24cf5bfbd5e93b17bbf140a44acd
Toivo <xan.asur@mail.ru>
2023-07-12 06:23:29 +0300

    Added Task 28

---
+++ Task28/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120 */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Произведение чисел от 1 до {number} -> {Mult(number)}");
+
+int Mult(int x)
+{
+    int mult = 1;
+    for(int i = 1; i <= x; i++)
+    {
+        checked
+        {
+            mult = mult * i;
+        }
+        
+    }
+    return mult;
+}

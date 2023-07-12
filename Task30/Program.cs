9706f929dd69011c00a41f998520f0464f49171c
Toivo <xan.asur@mail.ru>
2023-07-12 10:34:09 +0300

    Added Task 30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая
+выводит массив из 8 элементов, заполненный
+нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0] */
+
+int[] arrayNums = new int[8];
+
+FillArray(arrayNums);
+PrintArray(arrayNums);
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayNums[i] = new Random().Next(2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+     for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}

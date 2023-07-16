f77ad686c000adbb8581924b19ebd54977c05b56
Toivo <xan.asur@mail.ru>
2023-07-16 07:10:50 +0300

    Added task41

---
+++ Task41/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+
+Console.WriteLine("Введите числа с клавиатуры:");
+int count = 0;
+int timeVariable = 0;
+
+for (int i = 1; i > 0; i++)
+{
+    string timeString = Console.ReadLine();
+    if(timeString == "" || timeString == string.Empty) break;
+    else 
+    {
+        timeVariable = Convert.ToInt32(timeString);
+        if (timeVariable > 0) count++;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0, введеных пользователем: {count}");

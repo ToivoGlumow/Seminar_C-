79f911a0159dcd54be2f444a1472fa6572ded138
Toivo <xan.asur@mail.ru>
2023-07-17 12:06:15 +0300

    Added task49

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты. */
 
 int[,] array = CreateMatrixRndInt(4, 4, 0, 9);
 PrintMatrix(array);

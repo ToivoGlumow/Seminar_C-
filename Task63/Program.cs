b628ba2a916db53352c16331776177c17d8b630c
Toivo <xan.asur@mail.ru>
2023-07-29 07:00:00 +0300

    Added task63

---
+++ Task63/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 5 -> "1, 2, 3, 4, 5, 6" */
+
+
+Console.WriteLine("Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+SeqOutput(number);
+
+void SeqOutput(int x)
+{   
+   if(x == 0) return;
+   SeqOutput(x - 1);
+   Console.Write($"{x}");
+}
